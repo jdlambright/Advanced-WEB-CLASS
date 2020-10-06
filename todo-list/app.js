a13598e5b39a6d35406f8faa21ba27a5b8814fc5
jdlambright <64182772+jdlambright@users.noreply.github.com>
2020-10-05 21:40:27 -0400

    ejs template added

--- todo-list/app.js
+++ todo-list/app.js
@@ -5,8 +5,23 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+app.set("view engine", "ejs");
+
 app.get("/", function(req, res){
-  res.send("hello there");
+
+  var today = new Date();
+  var currentDay = today.getDay();
+  var day = "";
+
+  if (currentDay === 6 || currentDay === 0){
+    day = "Weekend";
+    // we are rendering a file called list which has to be located in views folder
+    //and have an extention .ejs and using a variable called day to put in index.html
+    res.render("list", {kindOfDay: day});
+  } else{
+    day = "Weekday";
+    res.render("list", {kindOfDay: day});
+  }
 });
 
 
