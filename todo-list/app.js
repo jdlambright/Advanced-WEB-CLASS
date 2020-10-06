13467a323421abdbbca1ec708ff84103b710fbfc
jdlambright <64182772+jdlambright@users.noreply.github.com>
2020-10-05 22:19:28 -0400

    added switch statements and scriplet tags

--- todo-list/app.js
+++ todo-list/app.js
@@ -7,25 +7,50 @@ const app = express();
 
 app.set("view engine", "ejs");
 
-app.get("/", function(req, res){
+app.get("/", function(req, res) {
 
   var today = new Date();
   var currentDay = today.getDay();
   var day = "";
 
-  if (currentDay === 6 || currentDay === 0){
-    day = "Weekend";
-    // we are rendering a file called list which has to be located in views folder
-    //and have an extention .ejs and using a variable called day to put in index.html
-    res.render("list", {kindOfDay: day});
-  } else{
-    day = "Weekday";
-    res.render("list", {kindOfDay: day});
+  // as a  general rule use switch statements if more than 5 items
+  // if less use if else {
+  switch (currentDay) {
+    case 0:
+      day = "Sunday";
+      break;
+    case 1:
+      day = "Monday";
+      break;
+    case 2:
+      day = "Tuesday";
+      break;
+    case 3:
+      day = "Wednesday";
+      break;
+    case 4:
+      day = "Thursday";
+      break;
+    case 5:
+      day = "Friday";
+      break;
+    case 6:
+      day = "Saturday";
+      break;
+    default:
+    console.log("error: current day is equal to: " + currentDay);
   }
+
+  // we are rendering a file called list which has to be located in views folder
+  //and have an extention .ejs and using a variable called day to put in index.html
+  res.render("list", {
+    kindOfDay: day
+  });
 });
 
+// scriplet tags in ejs file <% %> should only be used when you are trying to
+// modify a specific part of html for control flow purposes
 
-
-app.listen(3000,function(){
+app.listen(3000, function() {
   console.log("Boom Baby");
 });
