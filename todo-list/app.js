ee2a8e8ad531a3d8f2bd403c39acee18950a2314
jdlambright <64182772+jdlambright@users.noreply.github.com>
2020-10-05 22:43:54 -0400

    removed switch statement added object found on stackoverflow

--- todo-list/app.js
+++ todo-list/app.js
@@ -10,36 +10,15 @@ app.set("view engine", "ejs");
 app.get("/", function(req, res) {
 
   var today = new Date();
-  var currentDay = today.getDay();
-  var day = "";
 
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
   // as a  general rule use switch statements if more than 5 items
   // if less use if else {
-  switch (currentDay) {
-    case 0:
-      day = "Sunday";
-      break;
-    case 1:
-      day = "Monday";
-      break;
-    case 2:
-      day = "Tuesday";
-      break;
-    case 3:
-      day = "Wednesday";
-      break;
-    case 4:
-      day = "Thursday";
-      break;
-    case 5:
-      day = "Friday";
-      break;
-    case 6:
-      day = "Saturday";
-      break;
-    default:
-    console.log("error: current day is equal to: " + currentDay);
-  }
+  
 
   // we are rendering a file called list which has to be located in views folder
   //and have an extention .ejs and using a variable called day to put in index.html
