a659154e8320246d82e2caf5fa97dcad438cc8c5
jdlambright <64182772+jdlambright@users.noreply.github.com>
2020-10-31 16:05:37 -0400

    restarted

--- Wiki-API/app.js
+++ Wiki-API/app.js
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({
 app.use(express.static("public"));
 
 //this connects our server to the database
-mongoose.connect("mongodb://localhost:27017/wikidb", {useNewUrlParser: true});
+mongoose.connect("mongodb://localhost:27017/wikidb", {useUnifiedTopology: true,  useNewUrlParser: true});
 
 const articleSchema = {
   title: String,
@@ -22,7 +22,47 @@ const articleSchema = {
 
 const Article = mongoose.model("Article", articleSchema);
 
-//TODO
+//this gets the resources at /articles
+app.get("/articles", function(req,res){
+  Article.find(function(err, foundArticles){
+    if (!err){
+    res.send(foundArticles);
+  } else{
+    res.send(err);
+  }
+
+  });
+});
+
+//this builds the api
+app.post("/articles", function(req,res){
+  console.log();
+  console.log();
+
+  const newArticle = new Article({
+    title: req.body.title,
+    content: req.body.content
+  });
+  newArticle.save(function(err){
+    if (!err){
+      res.send("successfully saved new content");
+    }else{
+      res.send(err);
+    }
+  });
+});
+
+app.delete("/articles", function(req, res){
+  Article.deleteMany(function(err){
+    if (!err){
+      res.send("successfully deleted all articles");
+    } else {
+      res.send(err);
+    }
+  });
+});
+
+
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
