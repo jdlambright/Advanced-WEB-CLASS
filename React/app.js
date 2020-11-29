db1cfe64e8883e3ea1c8d12697436d2d1d47bbcd
jdlambright <64182772+jdlambright@users.noreply.github.com>
2020-11-29 15:57:26 -0500

    animation

--- React/app.js
+++ React/app.js
@@ -286,3 +286,43 @@ const newNumbers = numbers.map( x =>  x*x);
    </table>,
    document.getElementById("root")
  );
+
+
+// animation for buttons
+import React, { useState } from "react";
+
+function App() {
+  const [headingText, setHeadingText] = useState("hello");
+  const [isMouseOver, setMouseOver] = useState("false");
+
+  // how to change text
+  function handleClick() {
+    setHeadingText("submitted");
+  }
+
+  //how to change background color
+  function mouseOver() {
+    setMouseOver(true);
+  }
+
+  function mouseOut() {
+    setMouseOver(false);
+  }
+
+  return (
+    <div className="container">
+      <h1>{headingText}</h1>
+      <input type="text" placeholder="What's your name?" />
+      <button
+        style={{ backgroundColor: isMouseOver ? "black" : "white" }}
+        onClick={handleClick}
+        onMouseOver={mouseOver}
+        onMouseOut={mouseOut}
+      >
+        Submit
+      </button>
+    </div>
+  );
+}
+
+export default App;
