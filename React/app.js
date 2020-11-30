3b168ca7354511e585d61a24b7cd0a2fcb0be62e
jdlambright <64182772+jdlambright@users.noreply.github.com>
2020-11-29 19:03:27 -0500

    complex state changes

--- React/app.js
+++ React/app.js
@@ -359,3 +359,79 @@ function App() {
 }
 
 export default App;
+
+
+//complex state changes practice 2-----------------------------
+
+
+
+import React, { useState } from "react";
+
+function App() {
+  const [contact, setContact] = useState({
+    fName: "",
+    lName: "",
+    email: ""
+  });
+
+
+/// handleChange seems to be come language used but can be named anything
+  function handleChange(event) {
+    const { name, value } = event.target;
+// we get the name and value from input below around line 413
+// we get setContact from line 371
+    setContact((prevValue) => {
+      if (name === "fName") {
+        return {
+          fName: value,
+          lName: prevValue.lName,
+          email: prevValue.email
+        };
+      } else if (name === "lName") {
+        return {
+          fName: prevValue.fName,
+          lName: value,
+          email: prevValue.email
+        };
+      } else if (name === "email") {
+        return {
+          fName: prevValue.fName,
+          lName: prevValue.lName,
+          email: value
+        };
+      }
+    });
+  }
+
+  return (
+    <div className="container">
+      <h1>
+        Hello {contact.fName} {contact.lName}
+      </h1>
+      <p>{contact.email}</p>
+      <form>
+        <input
+          onChange={handleChange}
+          name="fName"
+          placeholder="First Name"
+          value={contact.fName}
+        />
+        <input
+          onChange={handleChange}
+          name="lName"
+          placeholder="Last Name"
+          value={contact.lName}
+        />
+        <input
+          onChange={handleChange}
+          name="email"
+          placeholder="Email"
+          value={contact.email}
+        />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
